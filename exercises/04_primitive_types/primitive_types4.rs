bebcb465a5a7e5df0790c626fd3fb7f8fde2c92c
Made Aryadinata <1260301+made-aryadinata@users.noreply.github.com>
2025-10-11 09:28:40 +0800

    first commit

---
+++ exercises/04_primitive_types/primitive_types4.rs
@@ -0,0 +1,23 @@
+fn main() {
+    // You can optionally experiment here.
+}
+
+fn slice(a: &[i32], n: usize) -> &[i32] {
+    &a[n..(a.len() - 1)]
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn slice_out_of_array() {
+        let a = [1, 2, 3, 4, 5];
+
+        // TODO: Get a slice called `nice_slice` out of the array `a` so that the test passes.
+        // let nice_slice = ???
+        let nice_slice = slice(&a, 1);
+
+        assert_eq!([2, 3, 4], nice_slice);
+    }
+}
---
+++ exercises/06_move_semantics/move_semantics4.rs
@@ -0,0 +1,26 @@
+fn main() {
+    // You can optionally experiment here.
+    let mut x = Vec::new();
+    let y = &mut x;
+    y.push(42);
+    y.push(12);
+    let z = &mut x;
+    z.push(13);
+    x.push(12);
+    println!("{:?}", x);
+}
+
+#[cfg(test)]
+mod tests {
+    // TODO: Fix the compiler errors only by reordering the lines in the test.
+    // Don't add, change or remove any line.
+    #[test]
+    fn move_semantics4() {
+        let mut x = Vec::new();
+        let y = &mut x;
+        y.push(42);
+        let z = &mut x;
+        z.push(13);
+        assert_eq!(x, [42, 13]);
+    }
+}

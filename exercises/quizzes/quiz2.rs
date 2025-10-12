f52fcdfa9776fbe9cef273f224916b17ca4cab41
Made Aryadinata <1260301+made-aryadinata@users.noreply.github.com>
2025-10-12 20:30:36 +0800

    quiz2

--- exercises/quizzes/quiz2.rs
+++ exercises/quizzes/quiz2.rs
@@ -27,7 +27,27 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function as described above.
-    // pub fn transformer(input: ???) -> ??? { ??? }
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
+        let mut result: Vec<String> = vec![];
+
+        for element in input {
+            let string = element.0;
+            let command = element.1;
+            match command {
+                Command::Uppercase => result.push(string.to_uppercase()),
+                Command::Trim => result.push(string.trim().to_owned()),
+                Command::Append(size) => {
+                    let mut suffix: String = String::from("");
+                    for _i in 0..size {
+                        suffix += "bar";
+                    }
+                    result.push(format!("{string}{suffix}"));
+                }
+            }
+        }
+
+        result
+    }
 }
 
 fn main() {
@@ -38,6 +58,7 @@ fn main() {
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
     // use ???;
+    use super::my_module::transformer;
     use super::Command;
 
     #[test]
